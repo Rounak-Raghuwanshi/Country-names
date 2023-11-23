76f1754017f47810298550c5f95ce827ed6caa3e
dheeraj_700 <dheeraj700agrahari@gmail.com>
2023-11-23 15:40:32 +0530

    theme_mode

--- src/components/FilterSection/filter.js
+++ src/components/FilterSection/filter.js
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
-import { IoMdSearch } from "react-icons/io";
-import { IoIosArrowDown } from "react-icons/io";
+import { IoMdSearch, IoIosArrowDown } from "react-icons/io";
 import { useTheme } from "../../ThemeContextSection/ThemeContext";
 
 const Filter = ({ onSearch, onFilter }) => {
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedRegion, setSelectedRegion] = useState("");
   const [dropdownOpen, setDropdownOpen] = useState(false);
-  const {isDarkTheme} = useTheme();
+  const { isDarkTheme } = useTheme();
 
   const handleChange = (e) => {
     setSearchTerm(e.target.value);
     onSearch(e.target.value);
   };
+
   const handleFilterChange = (region) => {
     setSelectedRegion(region);
     onFilter(region, searchTerm);
@@ -28,74 +28,90 @@ const Filter = ({ onSearch, onFilter }) => {
   return (
     <div className="mb-8 flex flex-col md:flex-row items-center justify-between relative">
       <div className="relative w-4/5 md:w-1/2 mb-4 md:mb-0 ">
-        <IoMdSearch className={`${isDarkTheme ? 'text-white': 'text-gray-600'} absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400`} />
+        <IoMdSearch
+          className={`${isDarkTheme ? "text-white" : "text-gray-600"} absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400`}
+        />
         <input
           type="text"
           placeholder="Search for a country..."
           value={searchTerm}
           onChange={handleChange}
-          className={` ${isDarkTheme ? 'bg-gray-700 text-white border-gray-800' : 'bg-white'} p-2 pl-10 w-full md:w-3/4 rounded-md focus:outline-none shadow-lg border border-gray-200`}
+          className={`${
+            isDarkTheme ? "bg-gray-700 text-white border-gray-800" : "bg-white"
+          } p-2 pl-10 w-full md:w-3/4 rounded-md focus:outline-none shadow-lg border border-gray-200`}
         />
       </div>
 
-      <div className="relative  md:w-1/4 w-4/5">
+      <div className="relative md:w-1/4 w-4/5">
         <div
-          className={`${isDarkTheme ? ' bg-gray-700 border-gray-800' : 'border-gray-200'} p-2 w-full rounded-md focus:outline-none shadow-lg cursor-pointer flex items-center border border-gray-200`}
+          className={`${
+            isDarkTheme ? "bg-gray-700 border-gray-800" : "border-gray-200"
+          } p-2 w-full rounded-md focus:outline-none shadow-lg cursor-pointer flex items-center border border-gray-200`}
           onClick={() => setDropdownOpen(!dropdownOpen)}
         >
-          <span className={`mr-2 md:ml-2`}>{selectedRegion || "Filter by Region"}</span>
+          <span className={`mr-2 md:ml-2`}>
+            {selectedRegion || "Filter by Region"}
+          </span>
           <IoIosArrowDown className="absolute right-10" />
         </div>
         {dropdownOpen && (
-          <div className={` ${isDarkTheme ? 'bg-gray-700 text-white border-gray-800' : 'bg-white'} absolute top-full left-0 mt-2 w-full bg-white rounded-md overflow-hidden shadow-lg`}>
-            <div
-              className={`p-2 md:ml-2 ${isDarkTheme ? 'hover:bg-gray-800' : 'hover:bg-gray-100'} cursor-pointer ${
-                selectedRegion === "Africa" ? "bg-gray-100" : ""
-              }`}
-              onClick={() => handleFilterChange("Africa")}
-            >
-              Africa
-            </div>
-            <div
-              className={`p-2 md:ml-2 ${isDarkTheme ? 'hover:bg-gray-800' : 'hover:bg-gray-100'} cursor-pointer ${
-                selectedRegion === "Africa" ? "bg-gray-100" : ""
-              }`}
-              onClick={() => handleFilterChange("Americas")}
-            >
-              Americas
-            </div>
-            <div
-             className={`p-2 md:ml-2 ${isDarkTheme ? 'hover:bg-gray-800' : 'hover:bg-gray-100'} cursor-pointer ${
-              selectedRegion === "Africa" ? "bg-gray-100" : ""
-            }`}
-              onClick={() => handleFilterChange("Asia")}
-            >
-              Asia
-            </div>
-            <div
-              className={`p-2 md:ml-2 ${isDarkTheme ? 'hover:bg-gray-800' : 'hover:bg-gray-100'} cursor-pointer ${
-                selectedRegion === "Africa" ? "bg-gray-100" : ""
-              }`}
-              onClick={() => handleFilterChange("Europe")}
-            >
-              Europe
-            </div>
-            <div
-              className={`p-2 md:ml-2 ${isDarkTheme ? 'hover:bg-gray-800' : 'hover:bg-gray-100'} cursor-pointer ${
-                selectedRegion === "Africa" ? "bg-gray-100" : ""
-              }`}
-              onClick={() => handleFilterChange("Oceania")}
-            >
-              Oceania
-            </div>
-            <div
-               className={`p-2 md:ml-2 ${isDarkTheme ? 'hover:bg-gray-800' : 'hover:bg-gray-100'} cursor-pointer`}
-               onClick={handleDefaultFilter}
-            >
-              Show All
-            </div>
-          </div>
-        )}
+  <div
+    className={`${
+      isDarkTheme
+        ? "bg-gray-700 text-white border-gray-800"
+        : "bg-white"
+    } absolute top-full left-0 mt-2 w-full rounded-md overflow-hidden shadow-lg`}
+  >
+    <div
+      className={`p-2 md:ml-2 cursor-pointer ${
+        isDarkTheme ? "hover:bg-gray-500" : "hover:bg-gray-100"
+      } ${selectedRegion === "Africa" }`}
+      onClick={() => handleFilterChange("Africa")}
+    >
+      Africa
+    </div>
+    <div
+      className={`p-2 md:ml-2 cursor-pointer ${
+        isDarkTheme ? "hover:bg-gray-500" : "hover:bg-gray-100"
+      } ${selectedRegion === "Americas"}`}
+      onClick={() => handleFilterChange("Americas")}
+    >
+      Americas
+    </div>
+    <div
+      className={`p-2 md:ml-2 cursor-pointer ${
+        isDarkTheme ? "hover:bg-gray-500" : "hover:bg-gray-100"
+      } ${selectedRegion === "Asia" }`}
+      onClick={() => handleFilterChange("Asia")}
+    >
+      Asia
+    </div>
+    <div
+      className={`p-2 md:ml-2 cursor-pointer ${
+        isDarkTheme ? "hover:bg-gray-500" : "hover:bg-gray-100"
+      } ${selectedRegion === "Europe" }`}
+      onClick={() => handleFilterChange("Europe")}
+    >
+      Europe
+    </div>
+    <div
+      className={`p-2 md:ml-2 cursor-pointer ${
+        isDarkTheme ? "hover:bg-gray-500" : "hover:bg-gray-100"
+      } ${selectedRegion === "Oceania"}`}
+      onClick={() => handleFilterChange("Oceania")}
+    >
+      Oceania
+    </div>
+    <div
+      className={`p-2 md:ml-2 cursor-pointer ${
+        isDarkTheme ? "hover:bg-gray-500" : "hover:bg-gray-100"
+      }`}
+      onClick={handleDefaultFilter}
+    >
+      Show All
+    </div>
+  </div>
+)}
       </div>
     </div>
   );
